2bdfab8801478d2b636eefdfa2231215d62453e1
zoyron <sgrarora3@gmail.com>
2020-05-12 01:54:55 +0530

    minor changes to app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, My name is Sagar Arora.</h1>
+    </div>
+  );
+};
+
+export default App;
