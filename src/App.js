c18c638d80a4da078c06351df9888141a012e53e
zoyron <sgrarora3@gmail.com>
2020-05-12 02:07:35 +0530

    added the email and password to the file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassword] = useState("");
   return (
     <div>
-      <h1>Hello, My name is Sagar Arora.</h1>
+      <input
+        type="text"
+        placeholder="Enter user name"
+        value={userName}
+        onChange={(keyPress) => {
+          setUserName(keyPress.target.value);
+        }}
+      />
+
+      <input
+        type="password"
+        placeholder="Enter your password"
+        value={passWord}
+        onChange={(keyPress) => {
+          setUserName(keyPress.target.value);
+        }}
+      />
+      <p>{userName}</p>
     </div>
   );
 };
